a1a0f64f2cc1d3f646f6e8b5ce1ba5aed3ef8c0b
Juan Fernando Cogollo Doria <cocodrilette@gmail.com>
2023-10-04 23:49:46 -0500

    Feat: Mainnet deployment set up

--- script/UWattVendor.s.sol
+++ script/UWattVendor.s.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import {UWattVendor} from "../src/UWattVendor.sol";
-import {SimpleERC20} from "../src/SimpleERC20.sol";
+import {ERC20UWATT} from "../src/ERC20UWATT.sol";
+import {ERC20USDT} from "../src/ERC20USDT.sol";
 
 contract UWattVendorScript is Script {
     function run() public {
@@ -20,9 +21,8 @@ contract UWattVendorScript is Script {
             usdtAddress = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F;
             newUWattOwner = 0xa846cb52cc481dd6E223b1Cd2AbD49f17120AddE;
         } else {
-            SimpleERC20 ERC20_uWatt = new SimpleERC20();
-            SimpleERC20 ERC20_USDT = new SimpleERC20();
-
+            ERC20UWATT ERC20_uWatt = new ERC20UWATT();
+            ERC20USDT ERC20_USDT = new ERC20USDT();
             uWattAddress = address(ERC20_uWatt);
             usdtAddress = address(ERC20_USDT);
             newUWattOwner = 0x3Bd208F4bC181439b0a6aF00C414110b5F9d2656;
@@ -30,11 +30,7 @@ contract UWattVendorScript is Script {
             ERC20_uWatt.mint(address(newUWattOwner), 100000 * 10 ** 18);
         }
 
-        UWattVendor uWattVendor = new UWattVendor(
-            uWattAddress,
-            usdtAddress,
-            newUWattOwner
-        );
+        new UWattVendor(uWattAddress, usdtAddress, newUWattOwner);
 
         vm.stopBroadcast();
     }
