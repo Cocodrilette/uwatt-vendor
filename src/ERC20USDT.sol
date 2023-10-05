974870e14353c736209c57bf8411d0542057e9c5
Juan Fernando Cogollo Doria <cocodrilette@gmail.com>
2023-10-04 23:50:28 -0500

    Feat: ERC20 test tokens updated

--- src/ERC20USDT.sol
+++ src/ERC20USDT.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract SimpleERC20 is ERC20 {
-    constructor() ERC20("SimpleERC20", "ERC20") {}
+contract ERC20USDT is ERC20 {
+    constructor() ERC20("ERC20USDT", "ERC20USDT") {}
 
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
     }
 }
